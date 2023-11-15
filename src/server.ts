f515af472e52a179c8d43cec071470d263bdfdd5
Kyrllan Nogueira <kyrllan@hotmail.com>
2023-11-15 15:40:26 -0300

    query params, route params and request body

--- src/server.ts
+++ src/server.ts
@@ -2,25 +2,37 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/users', (request, response) => {
-  return response.json({
-    users: ['usuario 1', 'usuario 2', 'usuario 3', 'usuario 4']
-   })
+
+  // Query params example
+  // const {perPage, currentPage} = request.query;
+  // return response.json({})
+
+  return response.json({ users: ['usuario 1', 'usuario 2', 'usuario 3'] })
+
 })
 
 app.post('/users', (request, response) => {
+  // Request body example
+  const body = request.body
+  console.log(body)
   return response.json({
     message: 'Criando usuário'
   })
 })
 
-app.put('/users', (request, response) => {
+app.put('/users/:id', (request, response) => {
+  // Route params example
+  const { id } = request.params
+  console.log(id)
   return response.json({
-    message: 'Atualizando usuário'
+    message: `usuário ${id} Atualizado`
   })
 })
 
-app.delete('/users', ( request, response) => {
+app.delete('/users', (request, response) => {
   return response.json({
     message: 'Deletando usuário'
   })
